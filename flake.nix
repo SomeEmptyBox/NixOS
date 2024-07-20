c7078cc6968afd8987872480ff5919ef26a2a583
SomeEmptyBox <121939750+SomeEmptyBox@users.noreply.github.com>
2024-07-21 03:43:08 +0530

    added disko

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
       url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
